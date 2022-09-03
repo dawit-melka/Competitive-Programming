e01935b4fc01591c0b8afb205f541d5ef468af28
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2022-09-04 00:46:22 +0300

    Create Selection-Sort.js

---
+++ Selection-Sort.js
@@ -0,0 +1,22 @@
+class Solution
+{
+  
+  //Function to sort the array using selection sort algorithm.
+  selectionSort(arr,n){
+   //code here
+   
+   for(let i=0; i<arr.length; i++){
+       let lowest=i
+       for(let j=i+1; j<arr.length; j++){
+           if(arr[j]<arr[lowest]) lowest=j;
+       }
+       if(i!=lowest){
+           let temp=arr[i];
+           arr[i]=arr[lowest];
+           arr[lowest]=temp;
+       }
+   }
+   return arr;
+  }
+    
+}
