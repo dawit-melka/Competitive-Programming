b785aafec800f3cd6db92eeb0e4e196bf257e4f8
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2022-09-15 02:05:48 +0300

    Create Reduce Array Size to The Half.js

---
+++ Reduce Array Size to The Half.js
@@ -0,0 +1,21 @@
+var minSetSize = function(arr) {
+    
+    let frequency = {}
+    let values =[]
+    let half = arr.length/2
+    let sum=0
+    
+    for(let val of arr){
+        frequency[val]=(frequency[val] || 0) +1
+    }
+    values= Object.values(frequency)
+    values.sort((a,b)=> b-a);
+    let count=values[0]
+    // if(values[0]>=half) return 1;
+    for(var i=0; i<values.length; i++){
+        sum += values[i]
+        if(sum>=half) break;
+    }
+    return i+1;
+    
+};
