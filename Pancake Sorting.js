8ac80d262283270679381e8dac227c40c8cfd392
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2022-09-15 01:20:20 +0300

    Create Pancake Sorting.js

---
+++ Pancake Sorting.js
@@ -0,0 +1,25 @@
+var pancakeSort = function(arr) {
+    let max = arr.length;
+    let k = [];
+    while(max>1){
+        
+        flip(arr.indexOf(max));
+        max--;
+    }
+    function flip(indx){
+        if(indx){
+            for(let i=0; i<indx/2; i++){
+                [[arr[i], arr[indx-i]] = [arr[indx-i], arr[i]]]
+            };
+            k.push(indx+1)
+        }
+        let len=max-1;
+        for(let i=0; i<len/2; i++){
+            [[arr[i], arr[len-i]] = [arr[len-i], arr[i]]]
+        };
+        k.push(max)
+    }
+    return k;
+    
+};
+
