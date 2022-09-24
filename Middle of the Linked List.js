02dd51a9de69fcb64f317110ace0fc58c3a4b133
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2022-09-25 00:01:57 +0300

    Update Middle of the Linked List.js

--- Middle of the Linked List.js
+++ Middle of the Linked List.js
@@ -1,15 +1,8 @@
 var middleNode = function(head) {
-    let length = 0;
-    let current = head;
-    while(current !== null){
-        current = current.next;
-        length++;
-    }
-    let half = Math.floor(length/2);
-    current = head;
-    for(let i = 0; i<half; i++){
-        current = current.next;
-    }
     
-    return current
+    function traverse(slow, fast) {
+        if(!fast || !fast.next) return slow;
+        return traverse(slow.next, fast.next.next);
+    }
+    return traverse(head, head);
 };
