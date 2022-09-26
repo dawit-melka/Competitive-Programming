b1d2076df955d41d24585b2c83fd8e7aa4255c57
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2022-09-27 00:22:00 +0300

    Update Remove Duplicates from Sorted List II.js

--- Remove Duplicates from Sorted List II.js
+++ Remove Duplicates from Sorted List II.js
@@ -2,19 +2,17 @@ var deleteDuplicates = function(head) {
     var current = head;
     var prev = head;
    
-    while(current){
-        if(!current.next) break;
+    while(current && current.next !== null){
+
         if(current.val === current.next.val){
             if(current === head){
-                while(current.val === current.next.val){
+                while(current.next !== null && current.val === current.next.val){
                     current = current.next;
-                    if(!current.next) return null;
                 } 
                 head = current.next;
             }else{
-                while(current.val === current.next.val){
+                while(current.next !== null && current.val === current.next.val){
                     current = current.next;
-                    if(!current.next) break;
                 } 
                 prev.next = current.next;
                 current = prev;
