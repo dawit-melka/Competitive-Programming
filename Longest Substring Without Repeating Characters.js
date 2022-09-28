382a12c54e0b069c0a3b148da3acde6b163fe047
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2022-09-28 18:02:36 +0300

    Update Longest Substring Without Repeating Characters.js

--- Longest Substring Without Repeating Characters.js
+++ Longest Substring Without Repeating Characters.js
@@ -1,18 +1,10 @@
 var lengthOfLongestSubstring = function(s) {
-    var map = new Map();
-    var left = 0, right = 0, max=0;
-    while(right < s.length){
-        if(map.get(s[right])){
-            map.set(s[right], map.get(s[right])+1)
-            while(map.get(s[right])>1){
-                map.set(s[left], map.get(s[left])-1);
-                if(map.get(s[left]) === 0) map.delete(s[left]);
-                left++;
-            }
-        }else map.set(s[right], 1);
-        
-        right++;
-        max = Math.max(max, right-left)
+    var seen = new Map();
+    var start = 0, max = 0;
+    for(let i=0; i<s.length; i++){
+        if(seen.has(s[i])) start = Math.max(seen.get(s[i])+1, start);
+        seen.set(s[i], i);
+        max = Math.max(max, i-start+1)
     }
     return max;
 };
