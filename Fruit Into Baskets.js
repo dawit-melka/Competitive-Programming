f73f689e4018076a38eeb1c80db58198f81dd343
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2022-09-28 15:21:31 +0300

    Update Fruit Into Baskets.js

--- Fruit Into Baskets.js
+++ Fruit Into Baskets.js
@@ -1,19 +1,18 @@
 var totalFruit = function(fruits) {
     var map = new Map();
-    var max = 0;
     var right = 0;
     var left = 0;
+    
     while(right < fruits.length){
         if(map.get(fruits[right])) map.set(fruits[right], map.get(fruits[right])+1);
         else map.set(fruits[right], 1);
         
-        while(map.size > 2){
+        if(map.size > 2){
             if(map.get(fruits[left]) === 1) map.delete(fruits[left]);
             else map.set(fruits[left], map.get(fruits[left])-1);
             left++;
         }
         right++;
-        max = Math.max(max, right-left);
     }
-    return max
+    return right-left;
 };
