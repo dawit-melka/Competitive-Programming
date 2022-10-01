6212178c7b1b013de94f9c5f67718b9a41c50c1f
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2022-10-01 20:50:15 +0300

    Delete Range Sum Query - Immutable.js

--- Range Sum Query - Immutable.js
+++
@@ -1,9 +0,0 @@
-var NumArray = function(nums) {
-        this.nums = nums
-        for(let i=1; i<nums.length; i++) this.nums[i]=this.nums[i] + this.nums[i-1];
-};
-
-NumArray.prototype.sumRange = function(left, right) {
-    if(left === 0) return this.nums[right];
-    else return this.nums[right] - this.nums[left-1];
-};
