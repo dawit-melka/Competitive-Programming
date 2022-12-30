ce0f0c3e356091ce32ff2812f2d0603e33377fd4
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2022-12-30 18:27:43 +0300

    Update Remove Comments.py
    https://leetcode.com/problems/remove-comments/description/

--- Remove Comments.py
+++ Remove Comments.py
@@ -1,31 +1,32 @@
 class Solution:
     def removeComments(self, source: List[str]) -> List[str]:
         res = []
-        look = False
+        opened = False
         temp = ""
+        
         for command in source:
-            if not look:
+            if not opened:
                 temp = ""
             n = len(command)
             i = 0
             while i < n:
-                if not look:
+                if not opened:
                     if i+1 < n and command[i] == "/" and command[i+1] == "/":
                         break
                     elif i+1 < n and command[i] == "/" and command[i+1] == "*":
                         i += 2
-                        look = True
+                        opened = True
                     else:
                         temp += command[i]
                         i += 1
                 else:
                     if i+1 < n and command[i] == "*" and command[i+1] == "/":
-                        look = False
+                        opened = False
                         i += 2
                     else: 
                         i += 1
             
-            if not look and len(temp) > 0:
+            if not opened and len(temp) > 0:
                 res.append(temp)      
 
         return res
