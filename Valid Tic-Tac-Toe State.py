d0560daef5ad0d8dc11e087207180447af9cd3b3
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2023-01-04 22:39:25 +0300

    Update Valid Tic-Tac-Toe State.py

--- Valid Tic-Tac-Toe State.py
+++ Valid Tic-Tac-Toe State.py
@@ -4,6 +4,7 @@ class Solution:
         win = ["XXX", "OOO"]
         self.winner = None
 
+        # function to check if both x and o winnes in a game
         def notValidWinner(string):
             if string in win:
                 if self.winner and self.winner != string:
@@ -13,6 +14,7 @@ class Solution:
             return False
 
         for row in board:
+            #  Check validity of row win
             if notValidWinner(row):
                 return False
             
@@ -21,11 +23,13 @@ class Solution:
                     count_x += 1
                 elif char == "O":
                     count_o += 1
-        
-        difference = count_x - count_o
-        if difference not in [0, 1]:
+
+        # valid number of moves is if X advance by one or equal to O
+        moves_gap = count_x - count_o
+        if moves_gap not in [0, 1]:
             return False
 
+        # Check validity of column win
         col1 = board[0][0]+board[1][0]+board[2][0]
         col2 = board[0][1]+board[1][1]+board[2][1]
         col3 = board[0][2]+board[1][2]+board[2][2]
@@ -37,6 +41,7 @@ class Solution:
         if notValidWinner(col3):
             return False
 
+        # check validity of diagonal win
         diagonal_1 = board[0][0]+board[1][1]+board[2][2]
         diagonal_2 = board[2][0]+board[1][1]+board[0][2]
 
@@ -45,9 +50,12 @@ class Solution:
         if notValidWinner(diagonal_2):
             return False
         
-        if self.winner=="XXX" and difference == 0:
+        # if the winner is X the number of moves of X has to exceed by 1
+        if self.winner=="XXX" and moves_gap == 0:
             return False
-        if self.winner=="OOO" and difference == 1:
+        
+        # if the winner is O the number of moves of X and O has to be equal
+        if self.winner=="OOO" and moves_gap == 1:
             return False
         
         return True
