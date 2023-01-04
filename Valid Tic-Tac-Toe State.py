8b0580dd4e28eab6a954ea55c7dcd48226e4ba63
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2023-01-04 22:18:08 +0300

    Update Valid Tic-Tac-Toe State.py

--- Valid Tic-Tac-Toe State.py
+++ Valid Tic-Tac-Toe State.py
@@ -1,56 +1,53 @@
 class Solution:
     def validTicTacToe(self, board: List[str]) -> bool:
-        count_X = 0
-        count_O = 0
-        winner = None
+        count_x, count_o = 0, 0
+        win = ["XXX", "OOO"]
+        self.winner = None
+
+        def notValidWinner(string):
+            if string in win:
+                if self.winner and self.winner != string:
+                    return True
+                else:
+                    self.winner = string
+            return False
 
         for row in board:
-            if row == "XXX" or row == "OOO":
-                if not winner:
-                    winner = row
-                elif winner != row:
-                    return False
+            if notValidWinner(row):
+                return False
+            
             for char in row:
                 if char == "X":
-                    count_X += 1
+                    count_x += 1
                 elif char == "O":
-                    count_O += 1
+                    count_o += 1
         
-        diff = count_X - count_O
-        if diff != 0 and diff !=1:
+        difference = count_x - count_o
+        if difference not in [0, 1]:
             return False
-        
-        for col in range(3):
-            curr_col = ""
-            for row in range(3):
-                curr_col += board[row][col]
-            if curr_col == "XXX" or curr_col == "OOO":
-                if not winner:
-                    winner = curr_col
-                else:
-                    if winner != curr_col:
-                        return False
 
-        diagonal_1 = board[2][0]+board[1][1]+board[0][2]
-        diagonal_2 = board[0][0]+board[1][1]+board[2][2]
-        if diagonal_1 == "XXX" or diagonal_1=="OOO":
-            if winner:
-                if winner != diagonal_1:
-                    return False
-            else:
-                winner = diagonal_1
-        
-        if diagonal_2 == "XXX" or diagonal_2=="OOO":
-            if winner:
-                if winner != diagonal_2:
-                    return False
-            else:
-                winner = diagonal_2
+        col1 = board[0][0]+board[1][0]+board[2][0]
+        col2 = board[0][1]+board[1][1]+board[2][1]
+        col3 = board[0][2]+board[1][2]+board[2][2]
 
-        if winner == "XXX" and diff == 0:
+        if notValidWinner(col1):
             return False
-        if winner == "OOO" and diff == 1:
+        if notValidWinner(col2):
             return False
+        if notValidWinner(col3):
+            return False
+
+        diagonal_1 = board[0][0]+board[1][1]+board[2][2]
+        diagonal_2 = board[2][0]+board[1][1]+board[0][2]
+
+        if notValidWinner(diagonal_1):
+            return False
+        if notValidWinner(diagonal_2):
+            return False
+        
+        if self.winner=="XXX" and difference == 0:
+            return False
+        if self.winner=="OOO" and difference == 1:
+            return False
+        
         return True
-        
-        
