1c4371eaa0018eb402ce4f74cc9ad53425d74a13
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2023-01-06 12:15:23 +0300

    Update Design Parking System.py

--- Design Parking System.py
+++ Design Parking System.py
@@ -1,21 +1,10 @@
 class ParkingSystem:
 
     def __init__(self, big: int, medium: int, small: int):
-        self.big = big
-        self.medium = medium
-        self.small = small  
+        self.slots = [0, big, medium, small]
 
     def addCar(self, carType: int) -> bool:
-        if carType == 1:
-            if self.big > 0:
-                self.big -= 1
-                return True
-        elif carType == 2:
-            if self.medium > 0:
-                self.medium -= 1
-                return True
-        elif carType == 3:
-            if self.small > 0:
-                self.small -= 1
-                return True
+        if self.slots[carType] > 0:
+            self.slots[carType] -= 1
+            return True
         return False
