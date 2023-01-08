a5b77d5958dc1bc77ca661049807cb7d20217775
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2023-01-08 20:37:56 +0300

    Update Partition Array According to Given Pivot.py

--- Partition Array According to Given Pivot.py
+++ Partition Array According to Given Pivot.py
@@ -1,16 +1,16 @@
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        result = []
-        for n in nums:
-            if n < pivot:
-                result.append(n)
+        n = len(nums)
+        result = [pivot]*(n)
+        low_idx = 0
+        high_idx = -1
 
-        for n in nums:
-            if n == pivot:
-                result.append(n)
-        
-        for n in nums:
-            if  n > pivot:
-                result.append(n)
+        for i in range(n):
+            if nums[i] < pivot:
+                result[low_idx] = nums[i]
+                low_idx += 1
+            if nums[-i-1] > pivot:
+                result[high_idx] = nums[-i-1]
+                high_idx -= 1
 
         return result
