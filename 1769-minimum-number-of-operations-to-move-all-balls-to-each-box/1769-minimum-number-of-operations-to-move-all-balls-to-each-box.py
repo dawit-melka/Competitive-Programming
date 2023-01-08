261756072f120e4b47bb838f0ec1403211bd19ca
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2023-01-08 11:56:14 +0300

    Update 1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py

--- 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
+++ 1769-minimum-number-of-operations-to-move-all-balls-to-each-box/1769-minimum-number-of-operations-to-move-all-balls-to-each-box.py
@@ -1,20 +1,24 @@
 class Solution:
     def minOperations(self, boxes: str) -> List[int]:
-        left_ones = 0
-        right_ones = 0
+        left_balls = 0
+        right_balls = 0
         left_operations = 0
         right_operations = 0
         min_operations = []
-
+        
+        # Get the total number of balls and operations required for i = 0
         for i, num in enumerate(boxes):
-            right_ones += int(num)
+            right_balls += int(num)
             right_operations += i* int(num)
-
+         
+        # starting from right iterate to left
+        # decrement right operations and right balls
+        # increament left operations and left balls
         for i in range(len(boxes)):
             min_operations.append(right_operations + left_operations)
-            right_ones -= int(boxes[i])
-            right_operations -= right_ones 
-            left_ones += int(boxes[i])
-            left_operations += left_ones
+            right_balls -= int(boxes[i])
+            right_operations -= right_balls 
+            left_balls += int(boxes[i])
+            left_operations += left_balls
 
         return min_operations
