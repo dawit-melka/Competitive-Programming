4cb0cb24436b66ee9b347fc5fc7bbccdc283cc83
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2023-01-09 16:42:52 +0300

    Update Find Consecutive Integers from a Data Stream.py

--- Find Consecutive Integers from a Data Stream.py
+++ Find Consecutive Integers from a Data Stream.py
@@ -3,15 +3,13 @@ class DataStream:
     def __init__(self, value: int, k: int):
         self.value = value
         self.k = k
-        self.count = 1
+        self.count = 0
         
     def consec(self, num: int) -> bool:
-        if num != self.value:
-            self.count = 1
-            return False
-        if self.count == self.k:
-            return True
+        if num == self.value:
+            self.count += 1
+        else:
+            self.count = 0
         
-        self.count += 1
-        return False
+        return self.count >= self.k
   
