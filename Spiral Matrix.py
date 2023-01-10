5c1d03f557f1b6d5a3f1bfb9d465d53befacd40d
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2023-01-10 19:30:27 +0300

    Update Spiral Matrix.py

--- Spiral Matrix.py
+++ Spiral Matrix.py
@@ -1,22 +1,24 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+        ROWS = len(matrix)
+        COLS = len(matrix[0])
         nextMove = {"right":"down", "down":"left", "left":"up", "up":"right"}
         move = {"right" : [0, 1], "down" : [1, 0], "left" : [0, -1], "up" : [-1, 0]}
         currMove = "right"
-        currRow = 0
-        currCol = 0
+        currRow, currCol = 0, 0
         visited = set()
-        res = []
-        ROWS = len(matrix)
-        COLS = len(matrix[0])
+        spiralOrder = []
 
-        while (len(res) < ROWS*COLS):
-            res.append(matrix[currRow][currCol])
+        while (len(spiralOrder) < ROWS*COLS):
+            spiralOrder.append(matrix[currRow][currCol])
             visited.add((currRow, currCol))
+
             i, j = move[currMove]
             nextRow = currRow + i
             nextCol = currCol + j
-            if (nextRow, nextCol) in visited or nextRow < 0 or nextRow == ROWS or nextCol < 0 or nextCol == COLS:
+            outOfRange = nextRow < 0 or nextRow == ROWS or nextCol < 0 or nextCol == COLS
+
+            if outOfRange or (nextRow, nextCol) in visited:
                 currMove = nextMove[currMove]
                 inxt, jnxt = move[currMove]
                 currRow += inxt
@@ -25,4 +27,4 @@ class Solution:
                 currRow += i
                 currCol += j
 
-        return res
+        return spiralOrder
