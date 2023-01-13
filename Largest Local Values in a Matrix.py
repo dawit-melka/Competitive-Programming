c8b9c30dee4c8d5f1b3973b5fe6d3a8f1275f6f0
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2023-01-13 10:48:47 +0300

    Update Largest Local Values in a Matrix.py

--- Largest Local Values in a Matrix.py
+++ Largest Local Values in a Matrix.py
@@ -3,12 +3,11 @@ class Solution:
         ROWS, COLS = len(grid), len(grid[0])
         maxLocal = [[0]*(COLS-2) for _ in range(ROWS-2)]
 
-        for r in range(1, ROWS-1):
-            for c in range(1, COLS-1):
-                maxVal = max(grid[r-1][c-1], grid[r-1][c], grid[r-1][c+1],
-                            grid[r][c-1], grid[r][c], grid[r][c+1],
-                            grid[r+1][c-1], grid[r+1][c], grid[r+1][c+1])
-                            
-                maxLocal[r-1][c-1] = maxVal
+        for r in range(ROWS-2):
+            for c in range(COLS-2):
+                maxVal = max(grid[r][c], grid[r][c+1], grid[r][c+2],
+                            grid[r+1][c], grid[r+1][c+1], grid[r+1][c+2],
+                            grid[r+2][c], grid[r+2][c+1], grid[r+2][c+2])
+                maxLocal[r][c] = maxVal
         
         return maxLocal
