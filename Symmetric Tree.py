b5879b717ad39eb82713e71445f5d2f376b114f1
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2023-03-08 10:47:16 +0300

    Update Symmetric Tree.py

--- Symmetric Tree.py
+++ Symmetric Tree.py
@@ -1,36 +1,11 @@
 class Solution:
     def isSymmetric(self, root: Optional[TreeNode]) -> bool:
-        
-        def notPlindrome(arr):
-            left, right = 0, len(arr)-1
-            while right > left:
-                if arr[left] != arr[right]:
-                    return True
-                left += 1
-                right -= 1
-            return False
-        
-        q = deque()
-        q.append(root)
-        while q:
-            currLevel = []
-            size = len(q)
-            while size:
-                size -= 1
-                node = q.popleft()
-                if node.left:
-                    currLevel.append(node.left.val)
-                    q.append(node.left)
-                else:
-                    currLevel.append(None)
-                    
-                if node.right:
-                    currLevel.append(node.right.val)
-                    q.append(node.right)
-                else:
-                    currLevel.append(None)
-            
-            if notPlindrome(currLevel):
+        def check(Lnode, Rnode):
+            if not Lnode and not Rnode:
+                return True
+            if not Lnode or not Rnode or Lnode.val != Rnode.val:
                 return False
-        
-        return True
+            
+            return check(Lnode.left, Rnode.right) and check(Lnode.right, Rnode.left)
+            
+        return check(root.left, root.right)
