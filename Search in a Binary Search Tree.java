0c1987e7f3ea1dd8d105d07c81e99cf86c90c4a9
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2023-03-08 11:42:24 +0300

    Create Search in a Binary Search Tree.java

---
+++ Search in a Binary Search Tree.java
@@ -0,0 +1,8 @@
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if(root == null) return null;
+        if(root.val == val) return root;
+
+        return root.val > val? searchBST(root.left, val) : searchBST(root.right, val);
+    }
+}
