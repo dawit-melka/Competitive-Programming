e7482d01995f56b246ddf0c2cd94ea4de9d98dc2
dawit-melka <105089130+dawit-melka@users.noreply.github.com>
2023-03-27 17:49:27 +0300

    Update Missing Number (cyclic sort).py

--- Missing Number (cyclic sort).py
+++ Missing Number (cyclic sort).py
@@ -4,9 +4,8 @@ class Solution:
         for i in range(len(nums)):
             while nums[i] != i+1 and nums[i] != -1:
                 if nums[i] == nums[nums[i]-1]:
-                    nums[i] = -1
-                else:
-                    nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
+                    break
+                nums[nums[i]-1], nums[i] = nums[i], nums[nums[i]-1]
 
         for i in range(len(nums)):
             if nums[i] != i + 1:
