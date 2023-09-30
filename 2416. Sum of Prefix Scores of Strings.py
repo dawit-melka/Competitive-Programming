255ece169fe206d9b4d125b2a0472291c09fa7c8
Dawit Badeg <105089130+dawit-melka@users.noreply.github.com>
2023-09-30 14:18:17 +0300

    Update 2416. Sum of Prefix Scores of Strings.py

--- 2416. Sum of Prefix Scores of Strings.py
+++ 2416. Sum of Prefix Scores of Strings.py
@@ -1,3 +1,44 @@
+# Approach 1: Trie
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.count = 0
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word):
+        currNode = self.root
+        for char in word:
+            if char not in currNode.children:
+                currNode.children[char] = TrieNode()
+            currNode = currNode.children[char]
+            currNode.count += 1
+    
+    def findPrefixScore(self, word):
+        count = 0
+        currNode = self.root
+        
+        for char in word:
+            currNode = currNode.children[char]
+            count += currNode.count
+        
+        return count
+        
+class Solution:
+    def sumPrefixScores(self, words: List[str]) -> List[int]:
+        trie = Trie()
+        result = []
+        for word in words:
+            trie.insert(word)
+        
+        for word in words:
+            result.append(trie.findPrefixScore(word))
+        
+        return result
+
+# Approach 2: Two pointers
 class Solution:
     def sumPrefixScores(self, words: List[str]) -> List[int]:
         sortedWords = sorted(words)
