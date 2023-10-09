511712f794b484f30f4b12224b494fb7822f560d
Dawit Badeg <105089130+dawit-melka@users.noreply.github.com>
2023-10-09 18:53:34 +0300

    Create 935. Knight Dialer.py

---
+++ 935. Knight Dialer.py
@@ -0,0 +1,29 @@
+class Solution:
+    def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
+        graph = defaultdict(list)
+        for i in range(len(equations)):
+            v1, v2 = equations[i]
+            graph[v1].append((v2, values[i]))
+            graph[v2].append((v1, 1/values[i]))
+        visited = set()
+        def dfs(currVar, target, ans):
+            if currVar not in graph:
+                return -1
+            if currVar == target:
+                return ans
+            
+            visited.add(currVar)
+            for child, mult in graph[currVar]:
+                if child not in visited:
+                    curr_ans = dfs(child, target, ans*mult)
+                    if curr_ans > 0:
+                        return curr_ans
+            
+            return -1
+        result = []
+        for src, dest, in queries:
+            visited = set()
+            result.append(dfs(src, dest, 1))
+
+        return result
+        
