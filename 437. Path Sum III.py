a5f71d5cda451b8ede8dbcd9b8b434f7a76adecb
Dawit Badeg <105089130+dawit-melka@users.noreply.github.com>
2023-10-10 11:45:04 +0300

    Create 437. Path Sum III.py

---
+++ 437. Path Sum III.py
@@ -0,0 +1,24 @@
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        if not root:
+            return 0
+        prevSum = defaultdict(int)
+        prevSum[0] = 1
+        result = 0
+        
+        def dfs(node, currSum):
+            nonlocal result
+            currSum += node.val
+            result += prevSum[currSum - targetSum]
+            prevSum[currSum] += 1
+
+            if node.left:
+                dfs(node.left, currSum)
+            if node.right:
+                dfs(node.right, currSum)
+            
+            prevSum[currSum] -= 1        
+            currSum -= node.val
+
+        dfs(root, 0)
+        return result
