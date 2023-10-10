248766bff103170668ecbfe78319f0c559c2d2d1
Dawit Badeg <105089130+dawit-melka@users.noreply.github.com>
2023-10-10 11:19:28 +0300

    Update 113. Path Sum II.py

--- 113. Path Sum II.py
+++ 113. Path Sum II.py
@@ -1,9 +1,4 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+# Approach 1:
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         self.allPath = []
@@ -22,3 +17,31 @@ class Solution:
 
         dfs(root, [])
         return self.allPath
+
+
+# Approach 2: Backtracking
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
+        if not root:
+            return []
+        
+        allPath = []
+        currPath = []
+        
+        def dfs(node, currSum):
+            currPath.append(node.val)
+            currSum += node.val
+            
+            if not node.left and not node.right and currSum == targetSum:
+                allPath.append(currPath.copy())
+            if node.left != None:
+                dfs(node.left, currSum)
+
+            if node.right != None:
+                dfs(node.right, currSum)
+
+            currPath.pop()
+            currSum -= node.val
+
+        dfs(root, 0)
+        return allPath
